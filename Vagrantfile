c569818f349ab221f023c334d69754ec03b1477c
wilas <limak.kamwil@gmail.com>
2012-11-25 20:46:43 +0100

    add jenkins module and headsman machine

--- Vagrantfile
+++ Vagrantfile
@@ -19,4 +19,20 @@ Vagrant::Config.run do |config|
     end
   end
 
+  config.vm.define :headsman do |config|
+    vm_name= "headsman"
+    config.vm.box = "SL64_box"
+    config.vm.host_name = "#{vm_name}.farm"
+    config.vm.customize ["modifyvm", :id, "--memory", "2048", "--name", "#{vm_name}"]
+
+    config.vm.network :hostonly, "77.77.77.132"
+    config.vm.share_folder "v-root", "/vagrant", "."
+
+    config.vm.provision :puppet do |puppet|
+        puppet.manifests_path = "manifests"
+        puppet.manifest_file  = "headsman.pp"
+        puppet.module_path = "modules"
+    end
+  end
+
 end
---
+++ modules/jenkins/manifests/init.pp
@@ -0,0 +1,48 @@
+class jenkins {
+
+    # repo key
+    file { "/etc/pki/rpm-gpg/jenkins-ci.org.key":
+        ensure => file,
+        owner  => "root",
+        group  => "root",
+        mode   => "0444",
+        source => "puppet:///modules/jenkins/jenkins-ci.org.key",
+    }
+
+    # jenkins repo
+    yumrepo { "jenkins":
+        enabled  => 1,
+        descr    => "Jenkins packages",
+        baseurl  => "http://pkg.jenkins-ci.org/redhat",
+        gpgcheck => 1,
+        gpgkey   => 'file:///etc/pki/rpm-gpg/jenkins-ci.org.key',
+        require  => File["/etc/pki/rpm-gpg/jenkins-ci.org.key"],
+    }
+
+    # install java -> 1.7.0 not working yet
+    package { "java":
+        name => ["java-1.6.0-openjdk", "java-1.6.0-openjdk-devel"],
+        ensure => installed,
+    }
+
+    package { "jenkins":
+        ensure  => installed,
+        require => [Yumrepo["jenkins"],Package["java"]],
+    }
+
+    service { "jenkins":
+        ensure     => running,
+        enable     => true,
+        hasrestart => true,
+        hasstatus  => true,
+        require    => Package["jenkins"],
+    }
+
+    firewall { "100 allow jenkins www":
+        state  => ['NEW'],
+        dport  => '8080',
+        proto  => 'tcp',
+        action => accept,
+    }
+
+}
