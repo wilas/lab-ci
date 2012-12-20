e4303b111e1b4d738e8e1dfbf6aa861327573137
wilas <limak.kamwil@gmail.com>
2012-12-20 03:26:05 +0100

    cleaning features/environment in beer_flask

--- samples/py_garden/py_beer_flask/features/environment.py
+++ samples/py_garden/py_beer_flask/features/environment.py
@@ -6,16 +6,14 @@ def before_all(context):
     context.client = beer.app.test_client()
 
 def before_tag(context, tag):
+    context.beer_json = beer.app.config['BEER_JSON']
     if 'beerdesc' in tag:
         beer.app.config['BEER_JSON'] = 'tests_json/test_beer_shortage.json'
     if 'encode' in tag:
         beer.app.config['BEER_JSON'] = 'tests_json/test_beer_encode.json'
 
 def after_tag(context, tag):
-    if 'beerdesc' or 'encode' in tag:
-        beer.app.config['BEER_JSON'] = 'beerpoint.json'
-    if 'encode' in tag:
-        beer.app.config['BEER_JSON'] = 'beerpoint.json'
+    beer.app.config['BEER_JSON'] = context.beer_json
 
 def after_all(context):
     pass
